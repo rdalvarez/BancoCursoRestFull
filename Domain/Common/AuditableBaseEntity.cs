ef827e0e939e153e2e8f6b15423941fd69ca916c
David Alvarez <david_de_odin@hotmail.com>
2025-02-14 14:38:54 -0300

    first commit

---
+++ Domain/Common/AuditableBaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+
+    //Entidad Base para que todas las entidades que creamos hereden de esta
+    public abstract class AuditableBaseEntity
+    {
+        public virtual int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+
+    }
+}
